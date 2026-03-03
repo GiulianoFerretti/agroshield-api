56ac729feac33dd3f3d34b74fc63c44f49f99e3f
GiulianoFerretti <123646225+GiulianoFerretti@users.noreply.github.com>
2026-03-03 11:35:08 -0400

    Update auth.py

--- app/auth.py
+++ app/auth.py
@@ -1,53 +1,53 @@
-from fastapi.security import OAuth2PasswordRequestForm
-from app.security import verify_password, create_access_token
-
 from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 from app.db import get_db
 from app.models import User
-from app.security import hash_password
+from app.security import hash_password, verify_password, create_access_token
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
-from pydantic import BaseModel
 
-class RegisterInput(BaseModel):
-    name: str
-    email: str
-    password: str
 @router.post("/register")
 def register(
-    payload: RegisterInput,
+    name: str,
+    email: str,
+    password: str,
     db: Session = Depends(get_db)
 ):
-    try:
-        existing = db.query(User).filter(User.email == payload.email).first()
-        if existing:
-            raise HTTPException(status_code=400, detail="Email já cadastrado")
+    existing = db.query(User).filter(User.email == email).first()
+    if existing:
+        raise HTTPException(status_code=400, detail="Email já cadastrado")
 
-        user = User(
-            name=payload.name,
-            email=payload.email,
-            password_hash=hash_password(payload.password),
-            role="admin",
-            is_active=True
-        )
+    user = User(
+        name=name,
+        email=email,
+        password_hash=hash_password(password),
+        role="admin",
+        is_active=True
+    )
 
-        db.add(user)
-        db.commit()
-        db.refresh(user)
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+
+    return {"message": "Usuário criado com sucesso"}
+
 
 @router.post("/login")
-def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+def login(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(get_db)
+):
     user = db.query(User).filter(User.email == form_data.username).first()
+
     if not user or not verify_password(form_data.password, user.password_hash):
         raise HTTPException(status_code=401, detail="Credenciais inválidas")
 
     token = create_access_token(subject=user.email)
-    return {"access_token": token, "token_type": "bearer"}
-        
-        return {"message": "Usuário criado com sucesso"}
 
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+    return {
+        "access_token": token,
+        "token_type": "bearer"
+    }
